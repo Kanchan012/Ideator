37f71d866aa01f7cd087e607e6e690e93af97d8f
Kanchan012 <lawojukanchan@gmail.com>
2026-01-04 16:15:03 +0000

    flash controller added

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,20 +1,16 @@
+# app/controllers/ideas_controller.rb
 class IdeasController < ApplicationController
   def index
-    @ideas = Idea.all
+    @pagy, @ideas = pagy(Idea.order(created_at: :desc), items: 5)
   end
 
-  def show
-    @idea = Idea.find(params[:id])
-  end
-
-  def create
+ def create
     @idea = Idea.create(idea_params)
-     @idea = Idea.create(idea_params)
-  if @idea.valid?
-    # Implement later
-  else
-    # Implement later
-  end
+    if @idea.valid?
+      flash[:success] = "Your idea has been posted!"
+    else
+      flash[:alert] = "Woops! Looks like there has been an error!"
+    end
     redirect_to root_path
   end
 
@@ -22,24 +18,27 @@ class IdeasController < ApplicationController
     @idea = Idea.find(params[:id])
   end
 
-  def update
+   def update
     @idea = Idea.find(params[:id])
     if @idea.update(idea_params)
+      flash[:success] = "The idea has been updated!"
       redirect_to root_path
     else
+      flash[:alert] = "Woops! Looks like there has been an error!"
       redirect_to edit_idea_path(params[:id])
     end
   end
 
-def destroy
-  @idea = Idea.find(params[:id])
-  @idea.destroy
-  redirect_to root_path
-end
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+    flash[:success] = "The idea was successfully deleted!"
+    redirect_to root_path
+  end
 
   private
 
   def idea_params
     params.require(:idea).permit(:description, :author)
   end
-end 
+end
